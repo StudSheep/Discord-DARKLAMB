8fbcf0f11573cfac29dee46859587bdd30ee6c07
Sheep <121650756+StudSheep@users.noreply.github.com>
2025-02-17 11:07:44 +0000

    update

--- main.py
+++ main.py
@@ -4,22 +4,27 @@ from json import dumps as JSONEncode
 from time import time
 
 intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-intents.presences = True
+intents.message_content = False
+intents.members = False
+intents.presences = False
 client = discord.client.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
 
 def ReadFile(path,obj):
-    file = open(path, "r")
-    content = file.read()
-    if content == '':
-        content = {}
+    try:
+        file = open(path, "r")
+    except:
+        open(path,"w").close()
+        return {}
     else:
-        content = JSONDecode(content)
-    if obj:
-        content = content[obj]
-    file.close()
+        content = file.read()
+        if content == '':
+            content = {}
+        else:
+            content = JSONDecode(content)
+        if obj:
+            content = content.get(obj)
+        file.close()
     return content
 
 def WriteFile(path,content):
@@ -45,14 +50,14 @@ def ModifyFile(path,obj,val):
     description="Claim your daily reward"
 )
 async def daily_command(interaction):
-    path = 'userdata/'+interaction.user_id
+    path = 'userdata/'+str(interaction.user.id)
     current = int(time())
     if (ReadFile(path,'daily_time') or 0) > current+(60*60*12):
-        await interaction.response.send_message(f"Your daily will be available <t:{current+(60*60*12)}:R>",ephemeral=True)
+        await interaction.response.send_message(f"Your daily will be available <t:{str(current+(60*60*12))}:R>",ephemeral=True)
         return
     streak = 1
     if (ReadFile(path,'daily_time') or 0) < current+(60*60*24):
-        streak = ReadFile(path,'daily_streak')+1
+        streak = (ReadFile(path,'daily_streak') or 0)+1
     ModifyFile(path,'daily_streak',streak)
     ModifyFile(path,'daily_time',current)
     gold = ReadFile(path,'gold') or 0
@@ -66,13 +71,13 @@ async def daily_command(interaction):
     description="Flex yo money"
 )
 async def balance_command(interaction):
-    path = 'userdata/'+interaction.user_id
+    path = 'userdata/'+str(interaction.user.id)
     gold = ReadFile(path,'gold') or 0
     await interaction.response.send_message(f"You have {str(gold)} gold, epic flex!!")
 
 @tree.command(
     name="duel",
-    description="Ask to duel another user"
+    description="This town ain't big enough for the two of us!"
 )
 async def duel_command(interaction,member: discord.Member):
     if not interaction.channel:
@@ -81,6 +86,9 @@ async def duel_command(interaction,member: discord.Member):
     view = discord.ui.View(timeout=15)
     view.add_item()
     acceptButton = discord.ui.Button(ButtonStyle=discord.ButtonStyle.primary,label='ACCEPT')
+    async def on_accept(interaction):
+        print('accepted',interaction.user.id)
+    acceptButton.callback = on_accept
     await interaction.response.send_message(interaction.user.mention+" challenged "+member.mention+" in a duel!",view=view)
 
 @client.event
